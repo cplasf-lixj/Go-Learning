501662290a4734bb04b942e939dad1de158a597f
李相举 <lixiangju@imohoo.com>
2021-04-22 11:53:02 +0800

    feat fixed bugs

--- Weak_02/modal/user.go
+++ Weak_02/modal/user.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 )
 
-func queryUserName(id int) string {
+func queryUserName(id int) (string, error) {
 	var username string
 	err := db.QueryRow("SELECT username FROM users WHERE id = ?", id).Scan(&username)
 	if nil != err {
 		if err == sql.ErrNoRows {
-			return ""
+			fmt.Printf("SQL ERROR: No user with user_id " + id)
+			return "", nil
 		}
-		fmt.Printf("SQL ERROR: No user with user_id " + id)
+		return "", err
 	}
-	return username
+	return username, nil
 }
